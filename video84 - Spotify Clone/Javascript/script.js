18cfbcb732889a1fec3080fa290164327ae10181
faisalzGIT <muhammadfaisal95610@gmail.com>
2024-12-16 22:34:23 +0530

    dt: 16-12-24

--- video84 - Spotify Clone/Javascript/script.js
+++ video84 - Spotify Clone/Javascript/script.js
@@ -1,15 +1,94 @@
 // JS
-console.log('Mohammd faisal')
 
+let currentSong = new Audio();
 
-async function main() {
-	let a = await fetch("http://127.0.0.1:5500/Audios/");
-	let response = await a.text();
-	
-	console.log(response)
-
-	
+function formatSeconds(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60); // Truncate decimals
+    return `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`;
 }
 
+async function getsongs() {
+	let a = await fetch("http://127.0.0.1:5500/Audios/");
+	let response = await a.text();
 
-main();
+	let div = document.createElement("div")
+	div.innerHTML = response;
+
+	let as = div.getElementsByTagName("a");
+
+	let songs = [];
+	console.log('i wannna say Alhamdulillah');
+
+	for (let index = 0; index < as.length; index++) {
+		const element = as[index];
+		if(element.href.endsWith(".mp3")){
+			songs.push(element.href.split("/Audios/")[1])
+		}
+	}
+	return songs;
+}
+
+//playMusic ka function
+const playMusic = (track, pause=false)=>{ 
+	currentSong.src = "/Audios/" + track;
+	if(!pause){
+		currentSong.play();
+		play.src = "/assets/images/pause.svg";
+	}
+
+	document.querySelector(".songinfo").innerHTML = track.replaceAll(".mp3","").replaceAll("-"," ");
+	document.querySelector(".songtime").innerHTML = "00:00";
+}
+
+async function main() {
+
+	// get the list o f all audios 
+	let songs = await getsongs(); 
+	// console.log(songs);
+	playMusic(songs[0], true);
+
+	let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
+
+	//show all the songs in  the playlist
+	for (const song of songs) {
+		songUL.innerHTML = songUL.innerHTML + `<li>
+			<div class="info">
+				<div>${song.replaceAll("-"," ").replace(".mp3","")}</div>
+				<div>Song Artist</div>
+			</div>
+			<div class="playnow">
+				<span>PLAY NOW</span>
+				<img src="/assets/images/play.svg" alt="playnowkisvg">
+			</div>
+		</li>`
+	}   
+
+	// attach an event listener to each song
+	Array.from(	document.querySelector(".songList").getElementsByTagName("li")).forEach(e => {
+		e.addEventListener('click',element=>{
+			// console.log(e.querySelector(".info").firstElementChild.innerHTML);
+			playMusic(e.querySelector(".info").firstElementChild.innerHTML.replaceAll(" ","-") + ".mp3")
+		})
+	})
+
+	// attach an EventListener to previous, play, next
+	play.addEventListener("click",()=>{
+		if(currentSong.paused){
+			currentSong.play();
+			play.src = "/assets/images/pause.svg"
+		} else{
+			currentSong.pause();
+			play.src = "/assets/images/play.svg"
+		}
+	})
+
+	// listen for time update event
+	currentSong.addEventListener("timeupdate",()=>{
+		console.log(currentSong.currentTime, currentSong.duration);
+		document.querySelector(".songtime").innerHTML = `${formatSeconds(currentSong.currentTime)}/ ${formatSeconds(currentSong.duration)}`
+	})
+} 
+
+main();   
+   
